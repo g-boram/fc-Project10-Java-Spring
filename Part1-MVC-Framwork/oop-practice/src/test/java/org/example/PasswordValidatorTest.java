88511400d1fb5566636e1b7de232318db5b6e253
GOBORAM <109337202+g-boram@users.noreply.github.com>
2024-09-17 16:32:45 +0900

    ch06.MVC 프레임워크 만들기

---
+++ Part1-MVC-Framwork/oop-practice/src/test/java/org/example/PasswordValidatorTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PasswordValidatorTest {
+
+    // 테스트 코드의 의도를 나타내주기
+    @DisplayName("비밀번호가 최소 8자 이상, 12자 이하면 예외가 발생하지 않는다.")
+    @Test
+    void name() {
+    }
+}
