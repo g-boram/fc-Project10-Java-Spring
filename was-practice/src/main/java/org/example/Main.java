87b7eb7be38843c9be6577b7e92917f257a458bb
GOBORAM <109337202+g-boram@users.noreply.github.com>
2024-09-17 11:11:03 +0900

    계산기 프로그램 웹 애플리케이션으로 만들기

--- was-practice/src/main/java/org/example/Main.java
+++ was-practice/src/main/java/org/example/Main.java
@@ -2,10 +2,9 @@ package org.example;
 
 import java.io.IOException;
 
-// GET /calculate?operand1=11&operator=*&operand2=55
-// GET 요청이고, 해당하는 패스는 calculate 이며, 피연산자 2개 연산자 1개를 전달하면 이에 대한 결과값을 리턴해준다.
 public class Main {
     public static void main(String[] args) throws IOException {
         new CustomWebApplicationServer(8080).start();
+        System.out.println("Aa");
     }
 }
