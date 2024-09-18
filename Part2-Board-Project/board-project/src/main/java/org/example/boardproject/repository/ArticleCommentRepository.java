f9583f91b6ecdf0d29c7625aa8fa8342777dffb2
GOBORAM <109337202+g-boram@users.noreply.github.com>
2024-09-18 21:33:18 +0900

    Spring data JPA 속성과 설정

---
+++ Part2-Board-Project/board-project/src/main/java/org/example/boardproject/repository/ArticleCommentRepository.java
@@ -0,0 +1,10 @@
+package org.example.boardproject.repository;
+
+
+import org.example.boardproject.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+}
---
+++ Part2-Board-Project/board-project/src/main/java/org/example/boardproject/repository/ArticleRepository.java
@@ -0,0 +1,10 @@
+package org.example.boardproject.repository;
+
+
+import org.example.boardproject.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
