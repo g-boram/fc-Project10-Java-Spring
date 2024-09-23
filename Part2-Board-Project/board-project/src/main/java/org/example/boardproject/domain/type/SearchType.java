9c04f8353e483788cbf3345c80dd11497996bda2
GOBORAM <109337202+g-boram@users.noreply.github.com>
2024-09-23 11:57:25 +0900

    게시판 페이지 검색바 기능 구현 + 페이징, 정렬과 연동

--- Part2-Board-Project/board-project/src/main/java/org/example/boardproject/domain/type/SearchType.java
+++ Part2-Board-Project/board-project/src/main/java/org/example/boardproject/domain/type/SearchType.java
@@ -1,6 +1,20 @@
 package org.example.boardproject.domain.type;
 
 
+import lombok.Getter;
+
+@Getter
 public enum SearchType {
-    TITLE, CONTENT, ID, NICKNAME, HASHTAG
+    TITLE("제목"),
+    CONTENT("본문"),
+    ID("유저 ID"),
+    NICKNAME("닉네임"),
+    HASHTAG("해시태그");
+
+    private final String description;
+
+    SearchType(String description) {
+        this.description = description;
+    }
+
 }
